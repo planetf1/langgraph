864e7682d558a8c57e4d6a491ead226b4ec051ef
Nigel Jones <jonesn@uk.ibm.com>
2025-01-15 14:46:32 +0000

    interim update
    Signed-off-by: Nigel Jones <jonesn@uk.ibm.com>

--- arxiv/qa.py
+++ arxiv/qa.py
@@ -25,35 +25,20 @@ tools = load_tools(
 
 llm = ChatOllama(model="llama3.1", tools=tools)
 
-template = '''Answer the following questions as best you can. You have access to the following tools:
+question = "What papers on quantum computing have been published since September 2024"
 
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question
-
-Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}'''
-
-
-question = "Give me a list of up to 10 papers from arxiv on quantum computing published since September 2024"
-
-#prompt = PromptTemplate.from_template(template)
 prompt = hub.pull("hwchase17/react")
-#agent = create_tool_calling_agent(llm, tools, prompt)
 agent = create_react_agent(llm, prompt=prompt,tools=tools)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True,handle_parsing_errors=False)
 
-output=agent_executor.invoke({"input": question})
+# Currently the search fails, since it reverts to using internal llm data and 2024 is too late
+# some prompt/template work needed to fix ...
+#result=agent_executor.invoke({"input": question})
 
-print(output)
+# Known working prompt - note this is a current article
+result=agent_executor.invoke(
+    {
+        "input": "What's the paper 2501.07744 about?",
+    }
+)
+print(result["output"])
